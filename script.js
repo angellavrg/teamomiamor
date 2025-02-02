983514cabf0f07cb36075e3cf52d41c55e06b270
angellavrg <angelmaciasnoriega1@gmail.com>
2025-02-01 18:42:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Me odias?",
+    "En serio??",
+    "Me quieres dejar?",
+    "Bebé :c",
+    "En serio...",
+    "Ya sabia que no me amas...",
+    "Estoy muy triste :(",
+    "Mi niña me odia :(",
+    "Está bien, te gusta otro",
+    "Eres mía, acepta ya ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
